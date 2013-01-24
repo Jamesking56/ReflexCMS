4f581c83e9bbc03c04e27a868c5083be4c9b47d3
James King <james@jamesking56.co.uk>
2013-01-24 19:36:20 +0000

    Attempting to fix TDD (Travis CI)

--- tests/Database.test.php
+++ tests/Database.test.php
@@ -6,18 +6,15 @@
  * Tests all Database class functions
  */
 define('IN_REFLEX',true);
-require_once '../src/includes/classes/Error.class.php';
 require_once '../src/includes/classes/Database.class.php';
 
 class DatabaseTest extends PHPUnit_Framework_TestCase
 {
 	private $db;
-	private $error;
 
 	// Setup suite
 	function __setUp()
 	{
-		$this->error = new Error(true, false, false);
 		$this->db = new Database($_ENV['DB_TYPE'], $_ENV['DB_HOST'], false, $_ENV['DB_USER'], $_ENV['DB_PASSWD'], $_ENV['DB_DBNAME']);
 	}
 	function __tearDown()
@@ -36,7 +33,6 @@ class DatabaseTest extends PHPUnit_Framework_TestCase
 	 */
 	public function testIfExceptionIsThrownWhenDatabaseCannotConnect()
 	{
-		$error = new Error(true);
 		$conn = new Database('mysql');
 	}
 }
